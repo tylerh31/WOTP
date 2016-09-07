eac1fbdd37dd3b65150fdbfaad2b75f7752c3aa0
Tyler Hansen <t.hansen@yashi.com>
2016-09-07 17:14:23 -0400

    [WIP] Switched to python3 from 2, requests from urllib2. Still working on subs and starters lists.

--- wotp.py
+++ wotp.py
@@ -2,13 +2,10 @@
 # -*- coding: latin-1 -*-
 # Tyler Hansen 8/1/2016
 from bs4 import BeautifulSoup
-import urllib2
 import re
 import sys
 import unicodedata
-
-reload(sys)  
-sys.setdefaultencoding('utf8')
+import requests
 
 links = []
 match_threads = []
@@ -28,13 +25,20 @@ def match_thread_info(url):
     team_2_subs = []
     team_1_subbed_in = []
     team_2_subbed_in = []
+    team_1_subbed_out = []
+    team_2_subbed_out = []
 
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    response = opener.open(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    html = requests.get(url, headers=headers)
 
-    req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-    html = urllib2.urlopen(req).read()
+    html = html.text
+
+    # opener = urllib2.build_opener()
+    # opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    # response = opener.open(url)
+
+    # req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+    # html = urllib2.urlopen(req).read()
 
     # MLS and soccer subreddits have different formatting, check to see which link this one is
     if ('/MLS/' in url):
@@ -63,78 +67,58 @@ def match_thread_info(url):
 
     process_team_subs(subs_team_2, team_2_subs)
 
-    print team1, 'vs.', team2
+    print (team1, 'vs.', team2)
 
     for sub in match_subs:
         subOut = sub[0].split(' ')
         # remove empty indexes in subOut
-        subOut = filter(None, subOut)
+        subOut = [x for x in subOut if x]
         # for players who only have 1 name (e.g. Kaka)
         if(len(subOut) == 1):
-            subOutName = remove_accents(subOut[0])
+            subOutName = subOut[0]
         elif(len(subOut) == 2):
-            subOutName = remove_accents(subOut[1])
+            subOutName = subOut[1]
         else:
-            subOutName = remove_accents(subOut[2])
+            subOutName = subOut[2]
 
         subIn = sub[1].split(' ')
         # remove empty indexes in subIn
-        subIn = filter(None, subIn)
+        subIn = [x for x in subIn if x]  
         # for players who only have 1 name (e.g. Kaka)
         if(len(subIn) == 1):
-            subInName = remove_accents(subIn[0])
+            subInName = subIn[0]
         elif(len(subIn) == 2):
-            subInName = remove_accents(subIn[1])
+            subInName = subIn[1]
         else:
-            subInName = remove_accents(subIn[2])
+            subInName = subIn[2]
 
-        print 'Off:', subOutName, 'On:', subInName
-
-        i = 0
-        j = 0
+        print ('Off:', subOutName, 'On:', subInName)
 
         # buggy with players having more than 2 names.
 
         for starter in team_1_starters:
             starter = starter.strip()
             starter = starter.split(' ')
-            if(len(starter) == 1):
-                if(subOutName == starter[0]):
-                    del team_1_starters[i]
-                    team_1_subbed_in.append(subInName)
-            elif(len(starter) == 2):
-                if(subOutName == starter[1]):
-                    del team_1_starters[i]
-                    team_1_subbed_in.append(subInName)
-            else:
-                if(subOutName == starter[2]):
-                    del team_1_starters[i]
-                    team_1_subbed_in.append(subInName)
-            i += 1
+            if(subOutName in starter):
+                team_1_subbed_out.append(starter)
 
         for starter in team_2_starters:
             starter = starter.strip()
             starter = starter.split(' ')
-            if(len(starter) == 1):
-                if(subOutName == starter[0]):
-                    del team_2_starters[j]
-                    team_2_subbed_in.append(subInName)
-            elif(len(starter) == 2):
-                if(subOutName == starter[1]):
-                    del team_2_starters[j]
-                    team_2_subbed_in.append(subInName)
-                    print subOutName
-            else:
-                if(subOutName == starter[2]):
-                    del team_2_starters[j]
-                    team_2_subbed_in.append(subInName)
-            j += 1
+            if(subOutName in starter):
+                team_2_subbed_out.append(starter)
 
-    team_1_players_on_pitch = team_1_starters + team_1_subbed_in
-    team_2_players_on_pitch = team_2_starters + team_2_subbed_in
+    for starter in team_1_starters:
+        a = [sub for sub in team_1_subbed_out if starter in team_1_subbed_out]
 
-    print team1, ':', team_1_players_on_pitch
-    print team2, ':', team_2_players_on_pitch
+    print(a)
+    exit()
+    print (team_1_starters, team_1_subbed_out)
+
+    exit()
+
+    print (team1, ':', team_1_players_on_pitch)
+    print (team2, ':', team_2_players_on_pitch)
 
 def get_team_1_name_starters_subs(html):
     result = re.search('.*<p><a href="#icon-notes-big"></a> <strong>LINE-UPS</strong></p>\s+<p><strong><a href="#\S+"></a>(.*?)</strong></p>\s+<p>(.*?).</p>\s+<p><strong>Subs:</strong>(.*?).</p>', html, re.DOTALL).groups()
@@ -162,43 +146,46 @@ def get_match_subs_MLS(html):
 def process_team_starters(unprocessed_list, processed_list):
     for starter in unprocessed_list:
         starter = starter.strip()
-        starter = remove_accents(starter)
         processed_list.append(starter)
 
 def process_team_subs(unprocessed_list, processed_list):
     for sub in unprocessed_list:
         sub = sub.strip()
-        sub = remove_accents(sub)
         processed_list.append(sub)
 
 def get_raw_match_links():
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    response = opener.open("http://www.reddit.com/u/MatchThreadder/?limit=50")
+    # opener = urllib2.build_opener()
+    # opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    # response = opener.open("http://www.reddit.com/u/MatchThreadder/?limit=50")
 
-    req = urllib2.Request("http://www.reddit.com/u/MatchThreadder/?limit=50", headers={ 'User-Agent': 'Mozilla/5.0' })
-    html = urllib2.urlopen(req).read()
-    soup = BeautifulSoup(html, "html.parser")
+    # req = urllib2.Request("http://www.reddit.com/u/MatchThreadder/?limit=50", headers={ 'User-Agent': 'Mozilla/5.0' })
+    # html = urllib2.urlopen(req).read()
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    html = requests.get("http://www.reddit.com/u/MatchThreadder/?limit=50", headers=headers)
+
+    soup = BeautifulSoup(html.text, "html.parser")
 
     # Get all links with class of "bylink comments may-blank" add to links[] list
     for link in soup.find_all('a', class_="bylink comments may-blank"):
-        href_pattern = re.compile(ur'.*href="(.*?)/".*')
+        href_pattern = re.compile('.*href="(.*?)/".*')
         comments_link = re.search(href_pattern, str(link)).groups()
         if('/MLS/' in comments_link[0]) or ('/soccer/' in comments_link[0]):
             match_links.append(comments_link[0])
 
 def prompt_user_for_game_link(match_links):
     i = 0
-    print 'Available Matches:\n'
+    print ('Available Matches:\n')
     for match_link in match_links:
         processed_match_link = parse_match_link(match_link)
-        print i, ':', processed_match_link[0]
+        print (i, ':', processed_match_link[0])
         i += 1
 
     desired_match_link = input("Choose the game you would like to see: ")
+    desired_match_link = int(desired_match_link)
 
-    if (desired_match_link not in range(len(match_links))):
-        print 'The game you have selected in not in the range of acceptable values, please try again'
+    if not(0 <= desired_match_link < len(match_links)):
+        print ('The game you have selected in not in the range of acceptable values, please try again')
         prompt_user_for_game_link(match_links)
 
     return desired_match_link
@@ -206,11 +193,6 @@ def prompt_user_for_game_link(match_links):
 def parse_match_link(match_link):
     result = re.search('.*/match_thread_(.*)', match_link, re.DOTALL).groups()
     return result
-
-def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', unicode(input_str))
-    only_ascii = nfkd_form.encode('ASCII', 'ignore')
-    return only_ascii
 
 get_raw_match_links()
 thread = prompt_user_for_game_link(match_links)
